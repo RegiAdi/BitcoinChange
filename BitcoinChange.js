00baa243ee23d42c256d8d847345edcf4f5fc4e7
Regi Adi <regiadi.priyanto@gmail.com>
2018-12-05 12:45:43 +0700

    Add bitcoin weekly historical data

--- BitcoinChange.js
+++ BitcoinChange.js
@@ -36,7 +36,65 @@ fetch('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD
     $('btc-volume24hourto').innerHTML = 'Volume24hourto: ' + data.VOLUME24HOURTO;
     $('btc-volumeday').innerHTML = 'Volumeday: ' + data.VOLUMEDAY;
     $('btc-volumedayto').innerHTML = 'Volumedayto: ' + data.VOLUMEDAYTO;
-    console.log(response.DISPLAY.BTC.USD);
+})
+.catch(function (error) {
+    console.log('Request Failed!', error);
+});
+
+fetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=6&api_key=' + apiKey)
+.then(function (response) {
+    return response.json();
+})
+.then(function (response) {
+    var data = response.Data;
+    console.log(data);
+
+    // show one week historical bitcoin data
+    var ul = $('btc-weekly-list');
+
+    Object.keys(data).forEach(function (key) {
+        var li = document.createElement('li');
+
+        var index = document.createTextNode(key);
+        li.appendChild(index);
+
+        var dailyDataList = document.createElement('ul');
+        var closeLi = document.createElement('li');
+        var highLi = document.createElement('li');
+        var lowLi = document.createElement('li');
+        var openLi = document.createElement('li');
+        var timeLi = document.createElement('li');
+        var volumefromLi = document.createElement('li');
+        var volumetoLi = document.createElement('li');
+
+        var close = document.createTextNode('Close: ' + data[key].close);
+        var high = document.createTextNode('High: ' + data[key].high);
+        var low = document.createTextNode('Low: ' + data[key].low);
+        var open = document.createTextNode('High: ' + data[key].open);
+        var time = document.createTextNode('Time: ' + data[key].time);
+        var volumefrom = document.createTextNode('Volumefrom: ' + data[key].volumefrom);
+        var volumeto = document.createTextNode('Volumeto: ' + data[key].volumeto);
+
+        closeLi.appendChild(close);
+        highLi.appendChild(high);
+        lowLi.appendChild(low);
+        openLi.appendChild(open);
+        timeLi.appendChild(time);
+        volumefromLi.appendChild(volumefrom);
+        volumetoLi.appendChild(volumeto);
+
+        dailyDataList.appendChild(closeLi);
+        dailyDataList.appendChild(highLi);
+        dailyDataList.appendChild(lowLi);
+        dailyDataList.appendChild(openLi);
+        dailyDataList.appendChild(timeLi);
+        dailyDataList.appendChild(volumefromLi);
+        dailyDataList.appendChild(volumetoLi);
+
+        li.appendChild(dailyDataList);
+
+        ul.appendChild(li);
+    });
 })
 .catch(function (error) {
     console.log('Request Failed!', error);
