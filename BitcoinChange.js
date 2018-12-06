4355a57701439ab6a0e3ae8f086e8ba665a0a01b
Regi Adi <regiadi.priyanto@gmail.com>
2018-12-06 11:03:35 +0700

    Add monthly bitcoin historical data and create unix timestamp to human readable date converter

--- BitcoinChange.js
+++ BitcoinChange.js
@@ -4,6 +4,14 @@ function $(element) {
     return document.getElementById(element);
 }
 
+function timeConverter(unixTimestamp) {
+    // multiply unix timestamp by 1000 to convert to miliseconds
+    var date = new Date(unixTimestamp * 1000);
+
+    return date;
+}
+
+// Get bitcoin trading info
 fetch('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD&api_key=' + apiKey)
 .then(function (response) {
     return response.json();
@@ -41,6 +49,7 @@ fetch('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD
     console.log('Request Failed!', error);
 });
 
+// Get weekly bitcoin historical data
 fetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=6&api_key=' + apiKey)
 .then(function (response) {
     return response.json();
@@ -51,6 +60,67 @@ fetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=6
 
     // show one week historical bitcoin data
     var ul = $('btc-weekly-list');
+
+    Object.keys(data).forEach(function (key) {
+        var li = document.createElement('li');
+
+        var index = document.createTextNode(key);
+        li.appendChild(index);
+
+        var dailyDataList = document.createElement('ul');
+        var closeLi = document.createElement('li');
+        var highLi = document.createElement('li');
+        var lowLi = document.createElement('li');
+        var openLi = document.createElement('li');
+        var timeLi = document.createElement('li');
+        var volumefromLi = document.createElement('li');
+        var volumetoLi = document.createElement('li');
+
+        var close = document.createTextNode('Close: ' + data[key].close);
+        var high = document.createTextNode('High: ' + data[key].high);
+        var low = document.createTextNode('Low: ' + data[key].low);
+        var open = document.createTextNode('High: ' + data[key].open);
+        var time = document.createTextNode('Time: ' + data[key].time);
+        var volumefrom = document.createTextNode('Volumefrom: ' + data[key].volumefrom);
+        var volumeto = document.createTextNode('Volumeto: ' + data[key].volumeto);
+
+        closeLi.appendChild(close);
+        highLi.appendChild(high);
+        lowLi.appendChild(low);
+        openLi.appendChild(open);
+        timeLi.appendChild(time);
+        volumefromLi.appendChild(volumefrom);
+        volumetoLi.appendChild(volumeto);
+
+        dailyDataList.appendChild(closeLi);
+        dailyDataList.appendChild(highLi);
+        dailyDataList.appendChild(lowLi);
+        dailyDataList.appendChild(openLi);
+        dailyDataList.appendChild(timeLi);
+        dailyDataList.appendChild(volumefromLi);
+        dailyDataList.appendChild(volumetoLi);
+
+        li.appendChild(dailyDataList);
+
+        ul.appendChild(li);
+    });
+})
+.catch(function (error) {
+    console.log('Request Failed!', error);
+});
+
+
+// Get monthly bitcoin historical data
+fetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=29&api_key=' + apiKey)
+.then(function (response) {
+    return response.json();
+})
+.then(function (response) {
+    var data = response.Data;
+    console.log(data);
+
+    // show one week historical bitcoin data
+    var ul = $('btc-monthly-list');
 
     Object.keys(data).forEach(function (key) {
         var li = document.createElement('li');
